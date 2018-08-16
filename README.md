a99f54c11f141fd3a6eff67d3128e301ff90b816
马特浩倪 <smile.used@hotmail.com>
2018-08-16 14:51:40 +0800

    setup

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# CAATsSQLServer
+# CAATs Tools
+
+Can't read any Chinese?
+See English Version
+
+>https://shields.io
+## 什么是CAATs？
+
+CAATs是计算机辅助审计（Computer Assisted Audit Techniques）的缩写。通常用于大规模数据的审计。
+
+## 这是什么
+
+CAATs Tools是一大堆工具的集合，有机结合，他使得你可以更加有效率地搞定CAATs之中恼人而又繁琐的工作。比如合并啊，拆分啊，简单判断呀等等。总之我们的目标就是尽量让你用一行代码搞定这些。
+
+我觉得这里应该有个目录，事实上，没有目录的话根本不知道要怎么去寻找你想要的东西。但我觉得首页应当保持干净整洁，所以目录的功能我放到了Wiki里面，请参阅。
+
+## 注意事项
+
+项目|版本|备注
+:--:|:--:|:--:
+SQL|SQL Server 2018|其它版本未做测试，如MySQL等其他SQl语言可能需要额外适配
+VBA|Microsoft Office 365|其它版本未做测试
+CMD|Windows 10|理论来讲，你可能需要管理员权限，也可能不要
+
+## 其它
+
+ 有好点子欢迎提issue，BUG也行咯
+
+ ----
---
+++ main.sql
@@ -0,0 +1,58 @@
+/*
+main.sql是一个基础的函数合集
+它包括一系列能够增加工作效率的函数
+只需要运行这个脚本即可自动添加这些函数
+****该文件适用于SQL Server****
+*/
+--这里面的函数可能会随着使用会不定期增加和优化
+--记得随时跟进运行此SQL脚本
+------------------------------------
+--目录
+------------------------------------
+/*─│├└********善用ctrl+F搜索********
+├─字符串操作
+  ├─1.切割字符串
+  │ └select dbo.cutStrByPar(<字符串>,<分割标志>,<分组序号>) FROM <TABLE>
+
+*/
+------------------------------------
+--初始化
+------------------------------------
+--删除已有函数，并重新创建他们，以免出现冲突（适用于算法优化）
+    DROP FUNCTION dbo.cutStrByPar --1.1
+
+------------------------------------
+--函数集
+------------------------------------
+--1.1 切割字符串
+    --返回字符串中特定的部分
+    --三个参数（<字符串><分隔符><部位编号>）
+    --用法select dbo.cutStrByPar([UID],',',2) FROM JE，意为：返回uid列字符串中以逗号分隔的第二组字符串
+        CREATE FUNCTION dbo.cutStrByPar(@target nvarchar(MAX)='0',@par nvarchar(255)=',',@order int)
+        RETURNS nvarchar(MAX) 
+        AS
+        begin
+            DECLARE @targetString nvarchar(MAX)
+            DECLARE @d int
+            DECLARE @c int
+            select @d=0
+            select @c=CHARINDEX(@par,@target)
+            while @order>1
+            begin
+                if @order=2
+                begin
+                    select @d=CHARINDEX(@par,@target,@c)
+                end
+                select @c=CHARINDEX(@par,@target,@c+1)
+                select @order=@order-1
+            end
+            if @c=0
+            begin
+                SELECT @targetString = right(@target,len(@target)-@d)
+            end
+            else
+            begin
+                SELECT @targetString = substring(@target,@d+1,@c-@d-1)
+            end
+            return @targetString
+        end
